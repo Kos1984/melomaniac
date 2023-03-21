59d9b7042bd3c85247000a82a9a6939d8efc3675
Kos1984 <86412949+Kos1984@users.noreply.github.com>
2023-03-21 13:35:42 +0300

    main changed

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,18 @@
-fun main () {
+fun main() {
     val purchaseLastMonth = true
-    val amount = 10001
+    val amount = 10000
     val discount = 100
     val percentOff = 5
     val percentOff2 = 1
     var totalPrise = amount
 
-
-    if (amount > 1000 && amount < 10000 ){
+    if (amount in 1000..10000) {
         totalPrise = amount - discount
-    }else if (amount > 10000){
-        totalPrise -= (totalPrise /100) * percentOff
+    } else if (amount > 10000) {
+        totalPrise -= (totalPrise / 100) * percentOff
     }
-    if (purchaseLastMonth){
-        totalPrise -= (totalPrise /100) * percentOff2
+    if (purchaseLastMonth) {
+        totalPrise -= (totalPrise / 100) * percentOff2
     }
     println(totalPrise)
 }
